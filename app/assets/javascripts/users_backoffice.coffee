7255ac734769ac272764a8c74cc763964aac86c0
Andre Pereira <andreporteira@gmail.com>
2022-05-19 14:09:10 -0300

    Bug em user_backoffice

--- app/assets/javascripts/users_backoffice.coffee
+++ app/assets/javascripts/users_backoffice.coffee
@@ -1,3 +1,13 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+//= require jquery-2/dist/jquery
+//= require bootstrap/dist/js/bootstrap.bundle.min
+//= require fastclick/lib/fastclick
+//= require nprogress/nprogress
+//= require custom.min
+//= require custom
+//= require malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar
+
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,7 +11,10 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css )
+Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css users_backoffice.css users_backoffice.js )
 
-Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css )
+#/lib/assets
+Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css  custom.min.css custom.css custom.min.js custom.js undraw_profile.svg undraw_profile_1.svg undraw_profile_2.svg undraw_profile_3.svg img.jpg font-awesome.min.css)
 
+# /vendor/assets
+Rails.application.config.assets.precompile += %w( jquery-2/dist/jquery.js )
