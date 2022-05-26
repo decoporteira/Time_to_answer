6c500811c0c4253b2062da2d4a2b1e9a0b9a8172
Andre Pereira <andreporteira@gmail.com>
2022-05-26 13:18:40 -0300

    Crindo novos admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
-  
   devise_for :users
   namespace :site do
-    get 'welcome/index'
+    get "welcome/index"
   end
   namespace :users_backoffice do
-    get 'welcome/index'
+    get "welcome/index"
   end
   namespace :admins_backoffice do
-    get 'welcome/index' #Dashboard
-    resources :admins, only: [:index, :edit, :update] # Administradores
+    get "welcome/index" #Dashboard
+    resources :admins, except: [:delete] # Administradores
   end
   devise_for :admins
-  
-  get 'inicio', to: 'site/welcome#index' # /inicio no navegador
 
-  root to: 'site/welcome#index'
+  get "inicio", to: "site/welcome#index" # /inicio no navegador
+
+  root to: "site/welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
