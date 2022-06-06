74d02083663d58fb826b30c80ce6fbf3b0a558b1
Andre Pereira <andreporteira@gmail.com>
2022-06-06 19:18:39 -0300

    criando controller subjects

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   namespace :admins_backoffice do
     get "welcome/index" #Dashboard
     resources :admins # Administradores
+    resources :subjects #assuntos/ areas
   end
   devise_for :admins
 
