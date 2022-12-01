1875334258115a4c8428c6e01a8c84ebabdae035
Andre Pereira <andreporteira@gmail.com>
2022-12-01 10:22:08 -0300

    Css Admin Devise

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,7 +11,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css users_backoffice.css users_backoffice.js )
+Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css users_backoffice.css users_backoffice.js admin_devise.css admin_devise.js )
 
 #/lib/assets
 Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css  custom.min.css custom.css custom.min.js custom.js undraw_profile.svg undraw_profile_1.svg undraw_profile_2.svg undraw_profile_3.svg img.jpg font-awesome.min.css)
