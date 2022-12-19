46536e05bc93a3b04b466cdc0c15eb92c891c970
Andre Pereira <andreporteira@gmail.com>
2022-12-19 10:21:20 -0300

    Search 1.0

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :site do
     get "welcome/index"
+    get "search", to: "search#questions"
   end
   namespace :users_backoffice do
     get "welcome/index"
